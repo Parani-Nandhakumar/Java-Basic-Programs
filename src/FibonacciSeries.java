002a20ff5280f382cfed2150f557d4960ac8ff14
Parani-Nandhakumar <parani777@gmail.com>
2020-11-21 16:32:03 +0530

    first commit

---
+++ src/FibonacciSeries.java
@@ -0,0 +1,16 @@
+public class FibonacciSeries {
+    public static void main(String[] args){
+        int number1=0;
+        int number2=1;
+        int number3;
+        int count=10;
+        System.out.println(number1);
+        System.out.println(number2);
+        for(int i=2;i<count;i++){
+            number3=number1+number2;
+            System.out.println(number3);
+            number1=number2;
+            number2=number3;
+        }
+    }
+}
